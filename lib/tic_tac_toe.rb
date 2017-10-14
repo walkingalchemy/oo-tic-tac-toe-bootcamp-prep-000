20403c185457a5d2dfd7e374fa0925d32955712c
James Royer <walkingalchemy@gmail.com>
2017-10-14 19:46:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,4 +57,16 @@ class TicTacToe
       "X" : "O"
   end
 
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+
 end
