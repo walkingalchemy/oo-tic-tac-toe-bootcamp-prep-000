cbd6914004796a43bda7d7c8d4f96538215e3703
James Royer <walkingalchemy@gmail.com>
2017-10-14 19:32:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,7 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
-  def display_board(@board)
+  def display_board(board)
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
