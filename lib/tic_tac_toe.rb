0c432a17ccddb0b30cc55e55c35110aced66bb79
James Royer <walkingalchemy@gmail.com>
2017-10-14 19:42:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    (index.between?(0,8) && !position_taken?(@board, index)) ?
+    (index.between?(0,8) && !position_taken?(index)) ?
     true : false
   end
 
