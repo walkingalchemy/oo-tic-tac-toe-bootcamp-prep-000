1f2632432ed3842bbc4fc9eb7927fba985a6f6ad
James Royer <walkingalchemy@gmail.com>
2017-10-14 19:44:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,4 +42,14 @@ class TicTacToe
     true : false
   end
 
+  def turn_count
+    count = 0
+    @board.each do |space|
+      if space == "X" || space == "O"
+        count += 1
+      end
+    end
+    return count
+  end
+
 end
