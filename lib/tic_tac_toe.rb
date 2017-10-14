23a8386a8bfe15e9492712a19447a232dd96943b
James Royer <walkingalchemy@gmail.com>
2017-10-14 19:55:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -100,4 +100,15 @@ class TicTacToe
      end
   end
 
+  def play
+    while !over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
 end
